4beaff7959c4e741993413aac26a74de1ec6245a
buli77 <kkulas310@gmail.com>
2022-09-19 12:03:29 +0200

    mongodb

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const mongo = require("mongodb");
 
 app = express();
 
 app.get("/", (req, res) => {
-    //res.send("Hello World!")
+    res.sendfile(__dirname + "/public/index.html");
 });
 
 app.use(express.static("public"));
 
-app.listen(8000);
+mongo.MongoClient.connect("mongodb://localhost:27017", async (err, db) => {
+    db.db("mydb").collection("posts").findOne({}, (err, res) => {
+        console.log(res);
+    })
+});
+
+app.listen(process.env.PORT || 8000);
